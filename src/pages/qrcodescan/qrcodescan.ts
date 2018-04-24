0e4614495914a5877c241b9cf3be0bb33b42a476
skeletonjoke <skeletonjoke84@gmail.com>
2018-04-24 15:02:12 +0200

    scan works

--- src/pages/qrcodescan/qrcodescan.ts
+++ src/pages/qrcodescan/qrcodescan.ts
@@ -1,19 +1,19 @@
   import { Component } from '@angular/core';
 
   import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+  import { Input } from '@angular/core';
  
-
-  
 
   @Component({
     selector: 'qrcodescan',
     templateUrl: 'qrcodescan.html'
   })
   export class QrcodeScanPage {
+    @Input() mode: string;
 
+    public state: string = "before"; //different possible states: before, config, started, baliseLoop, ..., ended, error
+    //en fonction du "state" on affiche la div qui correspond
 
-
-    
     constructor(private qrScanner : QRScanner) {
       
     } 
@@ -26,20 +26,27 @@
       this.qrScanner.prepare()
       .then((status: QRScannerStatus) => {
         if (status.authorized) {
+
           // camera permission was granted
-          alert('authorized');
+          console.log('camera authorized');
+          this.state = "config";
  
     
           // start scanning
           let scanSub = this.qrScanner.scan().subscribe((text: string) => {
             console.log('Scanned something', text);
-            alert(text);
+            console.log(text);
             this.qrScanner.hide(); // hide camera preview
-            scanSub.unsubscribe(); // stop scanning
+
+            this.state = "ended";// on viens de scanner la balise end
+
+            if(this.state == "ended"){
+              scanSub.unsubscribe(); // stop scanning
+            }
           });
 
           // select front camera 
-          //this.qrScanner.useFrontCamera();
+          this.qrScanner.useBackCamera();
 
           this.qrScanner.resumePreview();
 
@@ -49,18 +56,19 @@
           // wait for user to scan something, then the observable callback will be called
 
         } else if (status.denied) {
-          alert('denied');
+          console.log('denied');
           // camera permission was permanently denied
           // you must use QRScanner.openSettings() method to guide the user to the settings page
           // then they can grant the permission from there
         } else {
           // permission was denied, but not permanently. You can ask for permission again at a later time.
-          alert('else');
+          console.log('else');
         }
       })
       .catch((e: any) => {
-        alert('Error is' + e);
+        console.log('Error is' + e);
       });
 
     }
+
   }
